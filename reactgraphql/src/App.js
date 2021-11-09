6b3dee122ce5934d379d07502c06ff176b2131a7
Lewis Saunders <lewis-saunders@live.nmit.ac.nz>
2021-11-09 21:37:13 +1300

    halfwat through using the callback

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,24 +1,29 @@
 import github from "./db.js"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import query from "./Query"
 
 function App() {
   let [userName, setUserName] = useState("")
-    useEffect(()=> {
-      fetch(github.baseURL, {
-        method: "POST",
-        headers: github.headers,
-        body: JSON.stringify(query)
-      })
-        .then(response => response.json())
-        .then(data => {
-          setUserName(data.data.viewer.login)
-          console.log(data);
-        })
-        .catch(err => {
-          console.log(err);
-        })
+
+  const fetchData = useCallback( () => {
+    fetch(github.baseURL, {
+      method: "POST",
+      headers: github.headers,
+      body: JSON.stringify(query)
     })
+      .then(response => response.json())
+      .then(data => {
+        setUserName(data.data.viewer.login)
+        console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(()=> {
+    fetchData(() => {}, [fetchData]);
+  });
 
   return (
     <div className="App container mt-5">
