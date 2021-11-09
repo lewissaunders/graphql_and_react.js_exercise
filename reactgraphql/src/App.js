6008a53fc762200ea10c2451f35569d2378c3a7c
Lewis Saunders <lewis-saunders@live.nmit.ac.nz>
2021-11-09 21:29:07 +1300

    step completed

--- reactgraphql/src/App.js
+++ reactgraphql/src/App.js
@@ -1,9 +1,31 @@
+import github from "./db.js"
+import { useEffect, useState } from "react";
+import query from "./Query"
+
 function App() {
+  let [userName, setUserName] = useState("")
+    useEffect(()=> {
+      fetch(github.baseURL, {
+        method: "POST",
+        headers: github.headers,
+        body: JSON.stringify(query)
+      })
+        .then(response => response.json())
+        .then(data => {
+          setUserName(data.data.viewer.login)
+          console.log(data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+
   return (
     <div className="App container mt-5">
       <h1 className="text-primary">
         <i className="bi bi-diagram-2-fill"></i> Repos
       </h1>
+      <p>Hey there {userName} </p>
     </div>
   );
 }
